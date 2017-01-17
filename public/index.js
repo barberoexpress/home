165a302026a5ee6cc0f4ee2852e35fa626906924
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-17 17:15:50 -0500

    Cambio de imagen de la empresa

---
+++ public/index.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const config = require('./config')
+
+
+app.listen(config.port , function (){
+    console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
+    console.log("Desarrollado por: Cristian Franco Bedoya, Santiago R. Alvarez Gomez");
+    console.log("Ramla Pro. Software & Santiago R. Álvarez");
+});
